9f017b6bcb2282c9bb93c922cc1bb16819b80211
Ling Zhong <lingzhong@dhcp-vl2052-406.redrover.cornell.edu>
2021-11-30 15:53:03 -0500

    test

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -109,7 +109,7 @@ while True:
     # timer = strftime("%H:%M:%S")
     acc = str("Acc: %.2f, %.2f, %.2f " % (mpu.acceleration))
     gyr = str("Gyro: %.2f, %.2f, %.2f" % (mpu.gyro))
-
+    print(type(mpu.acceleration))
 
     font = getFont(20)
     
