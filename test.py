86bf8091fc267ba425f238f41b86fb22c10cd0d9
Ling Zhong <lingzhong@Lings-MacBook-Pro.local>
2021-12-03 06:56:15 -0500

    test

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
 from time import strftime, sleep
+import numpy as np
 from numpy import random, arctan2, sqrt, pi
 import board
 
@@ -98,12 +99,32 @@ maxAcc = 0
 fall = 0
 g = 9.81
 
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))/pi
+
+
 def streamAcc():
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     acc = str("Acc: %.2f, %.2f, %.2f " % (mpu.acceleration))
     accX, accY, accZ= mpu.acceleration[0], mpu.acceleration[1], mpu.acceleration[2]
     rmsAcc = sqrt(accX**2+accY**2+accZ**2)
+    orientation = unit_vector(mpu.acceleration)
 
     if rmsAcc >= maxAcc:
         maxAcc = rmsAcc
@@ -113,26 +134,36 @@ def streamAcc():
     y_1 = height/2 - font.getsize(acc)[1]/2
     draw.text((x_1, y_1), acc, font=font, fill="#FFFFFF")
 
-    strmaxAcc = 'Max: ' + str(maxAcc)
-    x_3 = width/2 - font.getsize(strmaxAcc)[0]/2
+    # strmaxAcc = 'Max: ' + str(maxAcc)
+    # x_3 = width/2 - font.getsize(strmaxAcc)[0]/2
+    # y_1 += font.getsize(acc)[1]
+    # draw.text((x_3, y_1), strmaxAcc, font=font, fill="#FFFFFF")
+
+    strrmsAcc = 'Max: ' + str(rmsAcc)
+    x_3 = width/2 - font.getsize(strrmsAcc)[0]/2
     y_1 += font.getsize(acc)[1]
-    draw.text((x_3, y_1), strmaxAcc, font=font, fill="#FFFFFF")
+    draw.text((x_3, y_1), strrmsAcc, font=font, fill="#FFFFFF")
 
     # Display image.
     disp.image(image, rotation)
     time.sleep(0.001)
 
-    return rmsAcc
+    return rmsAcc, orientation
 
 while True:
 
-    rmsAcc = streamAcc()
+    currAcc, orientation = streamAcc()
 
-    # if rmsAcc >= 2.5 * g:
-    #     accWindow = [0]*1000
-    #     i = 0
-    #     for x in accWindow:
-    #         accWindow[i] = rmsAcc
+    # if 0.72 * g <= currAcc <= 1.28 * g:
+
+
+    # if currAcc >= 2.5 * g:]
+
+    #     for x in range(1000):
+    #         currAcc, _ = streamAcc()
+    #     for x in range(1000):
+    #         currAcc = streamAcc()
+    #         if 0.72 * g <= currAcc <= 1.28 * g:
 
     # # Draw a black filled box to clear the image.
     # draw.rectangle((0, 0, width, height), outline=0, fill=0)
