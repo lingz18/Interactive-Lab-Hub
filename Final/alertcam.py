fc4bd5c0614b2ee735a6884c2e052e64bf5c94f1
Ling Zhong <lingzhong@Lings-MacBook-Pro.local>
2021-12-07 05:18:12 -0500

    test

--- Final/alertcam.py
+++ Final/alertcam.py
@@ -153,7 +153,7 @@ def draw_text(fontsize,strDraw,bgcolor):
 while True:
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    if fall:
+    if alertCam.fall:
         draw_text(25, 'Fall Detected', fill='red')
 
     time.sleep(0.01)
