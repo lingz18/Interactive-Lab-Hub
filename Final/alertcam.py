f2dae77a7fb151b6a8ef9bb2d9caea70428d4691
Ling Zhong <lingzhong@Lings-MacBook-Pro.local>
2021-12-07 05:15:28 -0500

    test

---
+++ Final/alertcam.py
@@ -0,0 +1,165 @@
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+from time import strftime, sleep
+import numpy as np
+import board
+
+import paho.mqtt.client as mqtt
+import uuid
+
+from adafruit_rgb_display.rgb import color565
+import adafruit_rgb_display.st7789 as st7789
+import webcolors
+
+from mail import sendEmail
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default Min is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape! 135
+width = disp.height #240
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 44)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+def getFont(size):
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", size)
+    return font
+
+def getRandomXY():
+    x = random.randint(240, size=10)
+    y = random.randint(135, size=10) 
+    return x, y
+
+x, y = getRandomXY()
+
+reset = True
+
+
+
+class App:
+    def __init__(self):
+        self.fall = False
+
+# the # wildcard means we subscribe to all subtopics of IDD
+topic = 'IDD/FallSafe/#'
+
+alertCam = App()
+
+
+#this is the callback that gets called once we connect to the broker. 
+#we should add our subscribe functions here as well
+def on_connect(client, userdata, flags, rc):
+    print(f"connected with result code {rc}")
+    client.subscribe(topic)
+    # you can subsribe to as many topics as you'd like
+    # client.subscribe('some/other/topic')
+
+
+# this is the callback that gets called each time a message is recived
+def on_message(cleint, userdata, msg):
+    print(f"topic: {msg.topic} msg: {msg.payload.decode('UTF-8')}")
+    alertCam.fall = ((msg.payload.decode('UTF-8'))=='True')
+    
+    # you can filter by topics
+    # if msg.topic == 'IDD/some/other/topic': do thing
+
+
+
+# attach out callbacks to the client
+client = mqtt.Client(str(uuid.uuid1()))
+client.tls_set()
+client.username_pw_set('idd', 'device@theFarm')
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+client.loop_start()
+
+#connect to the broker
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+# this is blocking. to see other ways of dealing with the loop
+#  https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php#network-loop
+client.loop_start()
+
+def draw_text(fontsize,strDraw,bgcolor):
+        draw.rectangle((0, 0, width, height), outline=0, fill=bgcolor)
+        font = getFont(fontsize)
+        x = width/2 - font.getsize(strDraw)[0]/2
+        y = height/2 - font.getsize(strDraw)[1]/2
+        draw.text((x, y), strDraw, font=font, fill="#FFFFFF")
+        disp.image(image, rotation)
+
+
+while True:
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    if fall:
+        draw_text(25, 'Fall Detected', fill='red')
+
+    time.sleep(0.01)
+                
